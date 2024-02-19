40543ccc0fd2c53d71fbf9f621d5fc509fdf9e03
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-02-19 15:31:55 -0300

    Add icons and update styling in App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { Octicons, FontAwesome5, FontAwesome, Ionicons, AntDesign, FontAwesome6 } from '@expo/vector-icons';
+
+
 
 export default function App() {
 
@@ -18,53 +21,37 @@ export default function App() {
       description: "API for Fortnite.",
       tecnologies: "React.js, JS, HTML5, css, Node.js",
     },
-    {
-      id: "1",
-      work: "Minecraft API",
-      img: require('./src/img/mine.png'),
-      description: "API for Minecraft.",
-      tecnologies: "React.js, JS, HTML5, css, Node.js",
-    },
-    {
-      id: "1",
-      work: "Minecraft API",
-      img: require('./src/img/mine.png'),
-      description: "API for Minecraft.",
-      tecnologies: "React.js, JS, HTML5, css, Node.js",
-    },
-    {
-      id: "1",
-      work: "Minecraft API",
-      img: require('./src/img/mine.png'),
-      description: "API for Minecraft.",
-      tecnologies: "React.js, JS, HTML5, css, Node.js",
-    },
-    {
-      id: "1",
-      work: "Minecraft API",
-      img: require('./src/img/mine.png'),
-      description: "API for Minecraft.",
-      tecnologies: "React.js, JS, HTML5, css, Node.js",
-    },
-    {
-      id: "1",
-      work: "Minecraft API",
-      img: require('./src/img/mine.png'),
-      description: "API for Minecraft.",
-      tecnologies: "React.js, JS, HTML5, css, Node.js",
-    },
+  
 
   ];
 
   return (
     <View className="bg-slate-50 min-h-screen flex flex-col justify-center items-center">
-      <View className="py-24 sm:py-32 text-center">
-        <Image source={require('./src/img/eu.jpg')} style={{ width: 64, height: 64 }} className="inline-block h-24 w-24 rounded-full ring-2 ring-white mb-4"></Image>
+      <View className="p-12 sm:py-32 flex flex-col justify-center items-center">
+        <Image
+          source={require('./src/img/eu.jpg')}
+          style={{ width: 100, height: 100 }}
+          className="inline-block h-24 rounded-full ring-2 ring-white mb-4"
+        />
+        <View className='flex flex-col justify-center items-center'>
         <Text className="text-2xl font-bold">Matheus Henrique de Carvalho Côcco</Text>
         <Text className="text-lg mb-2">TDS1 2° ano</Text>
         <Text className="text-gray-700">Sou um aluno de desenvolvimento de sistemas do Senai Valinhos.</Text>
+        </View>
       </View>
-
+      
+      <View className="bg-white p-4 rounded-md shadow-md mb-4 w-full max-w-md">
+        <Text>Tecnologias que eu ultilizo:</Text>
+        <View className="flex flex-row justify-center items-center">
+          <Octicons name="mark-github" size={24} color="black" />
+          <FontAwesome5 name="linkedin" size={24} color="black" />
+          <FontAwesome name="css3" size={24} color="black" />
+          <Ionicons name="logo-javascript" size={24} color="black" />
+          <AntDesign name="HTML" size={24} color="black" />
+          <FontAwesome6 name="node-js" size={24} color="black" />
+          <FontAwesome5 name="react" size={24} color="black" />
+          </View>
+      </View>
       <ScrollView className="p-4 w-full">
         <View className="flex flex-col items-center">
           <Text className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-4">Lista dos projetos:</Text>
@@ -85,6 +72,7 @@ export default function App() {
           )}
         </View>
       </ScrollView>
+      <StatusBar style="auto" />
     </View>
 
   );
